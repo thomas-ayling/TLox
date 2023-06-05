b13adf7e5f444d4336d52579dbf68587e436a0b0
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-06-05 08:42:45 +0100

    Scanner progress

---
+++ src/main/java/com/tom/tlox/TokenType.java
@@ -0,0 +1,27 @@
+package com.tom.tlox;
+
+public enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, SEMICOLON,
+
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+    SLASH, SLASH_SLASH, SLASH_EQUAL,
+    STAR, STAR_STAR, STAR_EQUAL,
+    MINUS, MINUS_MINUS, MINUS_EQUAL,
+    PLUS, PLUS_PLUS, PLUS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
