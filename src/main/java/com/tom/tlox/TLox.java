99a6bfa912ee1516d750d93c05dad9e329ff3f20
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-06-06 10:10:06 +0100

    Updated Expr

--- src/main/java/com/tom/tlox/TLox.java
+++ src/main/java/com/tom/tlox/TLox.java
@@ -22,36 +22,6 @@ public class TLox {
         }
     }
 
-//    Asp Printer test
-//    public static void main(String[] args) throws IOException {
-//        Expr expr = new Expr.Binary(
-//                new Expr.Unary(
-//                        new Token(TokenType.MINUS, "-", null, 1),
-//                        new Expr.Literal(123)),
-//                new Token(TokenType.STAR_EQUAL, "*=", null, 1),
-//                new Expr.Grouping(new Expr.Literal(45.67))
-//        );
-//    }
-
-//    Rpn Printer test
-//    public static void main(String[] args) throws IOException {
-//        Expr expr = new Expr.Binary(
-//                new Expr.Binary(
-//                        new Expr.Literal("1"),
-//                        new Token(TokenType.PLUS, "+", null, 1),
-//                        new Expr.Literal("2")
-//                ),
-//                new Token(TokenType.STAR, "*", null, 1),
-//                new Expr.Binary(
-//                        new Expr.Literal("4"),
-//                        new Token(TokenType.MINUS, "-", null, 1),
-//                        new Expr.Literal("3")
-//                )
-//        );
-//
-//        System.out.println(new RpnPrinter().print(expr));
-//    }
-
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
